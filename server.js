17411380bf1ed163ef50fd8860095361bed135b1
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-15 14:25:31 -0400

    serve static files

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
+// and then return the new note to the client. You'll need to find a way to give each note a unique
+// id when it's saved (look into npm packages that could do this for you).
+
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order
+// to delete a note, you'll need to read all notes from the db.json file, remove the note with the given
+// id property, and then rewrite the notes to the db.json file.

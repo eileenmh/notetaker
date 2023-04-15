6a323855358f70bf94cf1441c6208cc2cff241f4
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-15 17:36:07 -0400

    create a fallback route for when a user attempts to visit a route that doesn't exist

--- server.js
+++ server.js
@@ -5,23 +5,28 @@ const PORT = 3001;
 
 app.use(express.static("public"));
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
-
 // GET /notes should return the notes.html file.
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // GET * should return the index.html file.
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
 // and then return the new note to the client. You'll need to find a way to give each note a unique
 // id when it's saved (look into npm packages that could do this for you).
+app.post("/api/notes", (req, res) => {});
 
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order
 // to delete a note, you'll need to read all notes from the db.json file, remove the note with the given
 // id property, and then rewrite the notes to the db.json file.
+app.delete("/api/notes/:id", (req, res) => {});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);

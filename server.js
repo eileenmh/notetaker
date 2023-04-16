883d192cfb6641272ee2ecd4d504d3c2301d904a
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-15 22:02:37 -0400

    return db.json file on GET api/notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3001;
+const notesData = require("./db/db.json");
 
 app.use(express.static("public"));
 
@@ -10,12 +11,8 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// GET * should return the index.html file.
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
-);
-
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => res.json(notesData));
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
 // and then return the new note to the client. You'll need to find a way to give each note a unique
@@ -26,6 +23,11 @@ app.post("/api/notes", (req, res) => {});
 // to delete a note, you'll need to read all notes from the db.json file, remove the note with the given
 // id property, and then rewrite the notes to the db.json file.
 app.delete("/api/notes/:id", (req, res) => {});
+
+// GET * should return the index.html file.
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)

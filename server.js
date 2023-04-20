ad086007c9c051f57ac376dee526e57350c0d60a
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-19 23:22:11 -0400

    create DELETE route for deleting notes

--- server.js
+++ server.js
@@ -49,10 +49,24 @@ app.post("/api/notes", (req, res) => {
   res.status(201).json(response);
 });
 
-// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order
-// to delete a note, you'll need to read all notes from the db.json file, remove the note with the given
-// id property, and then rewrite the notes to the db.json file.
-app.delete("/api/notes/:id", (req, res) => {});
+// DELETE /api/notes/:id
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  let deleteNote = notesData.find((note) => note.note_id === noteId);
+  notesData.pop(deleteNote);
+
+  fs.writeFile("./db/db.json", JSON.stringify(notesData, null, 4), (writeErr) =>
+    writeErr
+      ? console.error(writeErr)
+      : console.info("Successfully saved note!")
+  );
+
+  const response = {
+    status: "success",
+  };
+
+  res.status(201).json(response);
+});
 
 // GET * should return the index.html file.
 app.get("*", (req, res) =>
